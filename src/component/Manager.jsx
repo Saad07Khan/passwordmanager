6495a69abe0d19733bfc5ef378e5d2f7412c4005
Saad Khan <saadmohammed.khan07@gmail.com>
2024-06-12 19:45:50 +0300

    functionalities for saving passsword to local storage

--- src/component/Manager.jsx
+++ src/component/Manager.jsx
@@ -1,40 +1,37 @@
 import React, { useEffect } from "react";
-import { useRef } from 'react';
+import { useRef, useEffect } from "react";
 import { useState } from "react";
 const Manager = () => {
-  const ref = useRef()
-  const [form, setform] = useState({site: "",username: "", password: ""})
-  const [passwordArray, setpasswordArray] = useState([])
+  const ref = useRef();
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setpasswordArray] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     let passwords = localStorage.getItem("passwords");
-       if(passwords){
-      setpasswordArray= JSON.parse(passwords)
+    if (passwords) {
+      setpasswordArray = JSON.parse(passwords);
     }
+  }, []);
 
-  },[])
-
-
-
-  const showPassword=()=>{
+  const showPassword = () => {
     alert("show the password");
-    if(ref.current.src.includes("icons/eyecross.png")){
-      ref.current.src="icons/eye.png"
+    if (ref.current.src.includes("icons/eyecross.png")) {
+      ref.current.src = "icons/eye.png";
+    } else {
+      ref.current.src = "icons/eyecross.png";
     }
-    else{
-      ref.current.src="icons/eyecross.png"
-    }
-  }
+  };
 
-  const savePassword = ()=>{
-   setpasswordArray([...passwordArray,form])
-   localStorage.setItem("password",JSON.stringify([...passwordArray,form]))
-  }
+  const savePassword = () => {
+    setpasswordArray([...passwordArray, form]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
+  };
 
-  const handleChange = (e) =>{
-    setform({...form, [e.target.name]: e.target.value})
-  }
-    
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
+  e.target;
   return (
     <>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
@@ -51,14 +48,20 @@ const Manager = () => {
           Your own secure Password Manager
         </p>
         <div className="flex flex-col p-4 text-black gap-8 items-center">
-          <input value={form.site} onChange = {handleChange} placeholder="Enter website URL"
+          <input
+            value={form.site}
+            onChange={handleChange}
+            placeholder="Enter website URL"
             className="rounded-full border border-green-500 w-full p-4 py-1 "
             type="text"
             name="site"
             id=""
           />
           <div className="flex w-full justify-between gap-3">
-            <input value={form.username} onChange = {handleChange}placeholder="Enter Username"
+            <input
+              value={form.username}
+              onChange={handleChange}
+              placeholder="Enter Username"
               className="rounded-full border border-green-500 w-full p-4 py-1"
               type="text"
               name="username"
@@ -66,23 +69,64 @@ const Manager = () => {
             />
 
             <div className="relative">
-            <input value={form.password} onChange = {handleChange}placeholder="Enter Password"
-              className="rounded-full border border-green-500 w-full p-4 py-1"
-              type="text"
-              name="password"
-              id=""
-            />
-            <span className='absolute right-[4px] top-[4px] cursor-pointer' onClick={showPassword}><img ref ={ref}className='p-1'width={23}src="icons/eye.png"/></span>
+              <input
+                value={form.password}
+                onChange={handleChange}
+                placeholder="Enter Password"
+                className="rounded-full border border-green-500 w-full p-4 py-1"
+                type="text"
+                name="password"
+                id=""
+              />
+              <span
+                className="absolute right-[4px] top-[4px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img ref={ref} className="p-1" width={23} src="icons/eye.png" />
+              </span>
             </div>
-            
           </div>
-          <button onClick = {savePassword}className="flex justify-center items-center bg-slate-500 rounded-full px-2 py-2 w-fit hover:bg-green-300">
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center bg-slate-500 rounded-full px-2 py-2 w-fit hover:bg-green-300"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
               trigger="hover"
             ></lord-icon>
             Add Password
           </button>
+        </div>
+        <div className="passwords">
+          <h2>Your Passwords</h2>
+          <table>
+            <table className="table-auto">
+              <thead>
+                <tr>
+                  <th>Song</th>
+                  <th>Artist</th>
+                  <th>Year</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                  <td>Malcolm Lockyer</td>
+                  <td>1961</td>
+                </tr>
+                <tr>
+                  <td>Witchy Woman</td>
+                  <td>The Eagles</td>
+                  <td>1972</td>
+                </tr>
+                <tr>
+                  <td>Shining Star</td>
+                  <td>Earth, Wind, and Fire</td>
+                  <td>1975</td>
+                </tr>
+              </tbody>
+            </table>
+          </table>
         </div>
       </div>
     </>
